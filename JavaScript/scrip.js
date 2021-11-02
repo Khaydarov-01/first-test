5e29e68ee0ea5a235487812b2495dbbe5480c0ae
Khaydarov-01 <Khaydarovdilshod9@gmail.com>
2021-11-02 15:36:12 +0500

    first commit

---
+++ JavaScript/scrip.js
@@ -0,0 +1,111 @@
+// let kasb = 'dasturchi';
+// let question = 'ismingizni kiriting';
+// let age = 'yoshingiz nechida';
+// const yosh = 25;
+
+// console.log(kasb, yosh);
+// console.log(question);
+// document.write(5+6);
+// alert(5+8); 
+
+// let age= 18;
+// if(age > 18){
+//     alert('Hush Kelibsiz');
+// }
+// if(age==18){
+//     prompt("Tug'ilgan oyingizni kiriting");
+// }
+// if(age < 18){
+//     alert("Siz balog'at yoshiga yetmagansiz");
+// }
+// else{
+//     alert("Error");
+// }
+          //Homeworks-----------------------------------------------
+// let gradus = 15;
+
+// if(gradus<=18 && gradus > 10){
+//     console.log("Weather is so good");
+// } else
+//  if(gradus <= 10 && gradus > 4 ){
+//  console.log("Be ceraful, it maybe rain!");
+// }else
+//  if(gradus <=4 && gradus > 0 ){
+//    console.log("Weather is very dangerous to drive a car");
+// }else   
+//  if(gradus <= 0 && gradus > -10 ){
+//     console.log("Don't leave your home");
+// }
+// else{
+//     console.log("Error");
+// }
+ 
+// let a=15; 
+// let b=14;
+// let c=12;
+// if ( c > a && c < b ){               
+//     alert("1-Tengsizlik qanoatlantirildi.");
+// }
+// if(b < c && a > c){
+//    alert("2-tengsizlik qanoatlantirildi.");    
+// }
+// if(a >= c && b <= a){
+//    alert("3-tengsizlik qanoatlantirildi.");
+// }
+// if(b < a && c > b){
+//     alert("4-tengsizlik qanoatalntirildi.");
+// }
+// if(c==b && b > a){
+//     alert("5-tengsizlik qanoatlantirildi.");
+// }
+// else{
+//     alert("Error");
+// }
+
+// let a="qizil";
+
+// if(a=="yashil"){
+//     alert("Yurishingiz mumkin");
+// }
+// if(a=="sariq"){
+//     alert("Yurishga tayyorlaning");
+// }
+// if(a=="qizil"){
+//     alert("Yurishingiz mumkin emas!!!");
+// }
+// else{
+//     alert("Error");
+// }
+
+//massives ------------------------------------------------------
+
+// let cars = ['Audi', 'BMW', 'Wolksvagen', 'Camaro', 'Nexia'];
+// if(cars.length==5){
+//     console.log("Mashinalar soni 4 ta");
+// }
+// else{
+//     console.log(cars.length);
+// }
+// let x=
+// +prompt("yoshingiz nechi?");
+// console.log(x);
+
+// let names=prompt("ismingiz nima");
+// if(names=='ali'){
+//     console.log("Xush kelibsiz");
+// }
+// else{
+//     console.log("Yo'qol");
+// }
+
+let names=prompt("Ismingiz nima?");
+let width=names.length;
+if(names==names){
+    console.log("Xush kelibsiz");
+}
+else if(width <= 4){
+    console.log("Yo'qol");
+}
+else{
+    console.log("Error");
+}
